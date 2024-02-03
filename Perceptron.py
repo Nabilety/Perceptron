e77a180a1c359c7f7af47ec6f64d53db8ecff8d1
Nabilety <37532159+Nabilety@users.noreply.github.com>
2024-02-03 08:00:26 +0100

    Implementing an Adaline learning algorithm in Python

--- Perceptron.py
+++ Perceptron.py
@@ -48,20 +48,22 @@ class Perceptron:
          self : object
 
          """
-
+        #print(X.shape[1])
+        #print(X)
+        # NOTE: X provides us with a list of list with values, i.e. [[5.1, 1.4] [4.9, 1.4]]
         rgen = np.random.RandomState(self.random_state) # Seed used to recall our weight w below  for next iteration Δw
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1]) # X.shape[1] extracts 2nd element in a tuple, meaning number of features
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1]) # X.shape[0] extract the dimension of the outer list, so it will return 100 being the number of examples. X.shape[1] extracts the dimension of 2nd element, meaning number of features for each example which is 2.
         self.b_ = np.float_(0.)
         self.errors_ = []
 
         for _ in range(self.n_iter):
             errors = 0
-            for xi, target in zip(X, y):
+            for xi, target in zip(X, y): # here xi is sepal and petal length vector, y target
                 update = self.eta * (target - self.predict(xi))
                 self.w_ += update * xi
                 self.b_ += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+                errors += int(update != 0.0) # if target vs predicted is misclassified, add 1
+            self.errors_.append(errors) # at the end of each epoch, append the number of misclassifications
         return self
 
     def net_input(self, X):
@@ -87,7 +89,7 @@ y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', 0, 1)
 
 # extract sepal length and petal length
-X = df.iloc[0:100, [0, 2]].values
+X = df.iloc[0:100, [0, 2]].values # example xi = [5.1 1.4]
 
 # plot data
 plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='Setosa')
@@ -142,3 +144,250 @@ plt.legend(loc='upper left')
 plt.show()
 
 
+
+# Implementing an Adaptive linear neuron (Adaline)
+class AdalineGD:
+    """ADAptive LInear NEuron classifier.
+
+     Parameters
+     ------------
+     eta : float
+        Learning rate (between 0.0 and 1.0)
+     n_iter : int
+        Passes over the training dataset.
+     random_state : int
+        Random number generator seed for random weight initialization.
+
+     Attributes
+     -----------
+     w_ : 1d-array
+        Weights after fitting.
+     b_ : Scalar
+        Bias unit after fitting.
+     losses_ : list
+        Mean squared error loss function values in each epoch.
+
+     """
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.1, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (errors**2).mean()
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada1 = AdalineGD(n_iter=15, eta=0.1).fit(X, y)
+ax[0].plot(range(1, len(ada1.losses_) + 1), np.log10(ada1.losses_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Mean squared error)')
+ax[0].set_title('Adaline - Learning rate 0.1')
+
+ada2 = AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.losses_) + 1), ada2.losses_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Mean squared error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.show()
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada_gd = AdalineGD(n_iter=20, eta=0.5)
+ada_gd.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada_gd)
+plt.title('Adaline - Gradient descent')
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('Petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+#plt.savefig('images/02_14_1.png', dpi=300)
+plt.show()
+
+plt.plot(range(1, len(ada_gd.losses_) + 1), ada_gd.losses_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Mean squared error')
+
+plt.tight_layout()
+#plt.savefig('images/02_14_2.png', dpi=300)
+plt.show()
+
+
+class AdalineSGD:
+    """ADAptive LInear NEuron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    shuffle : bool (default: True)
+      Shuffles training data every epoch if True to prevent cycles.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    b_ : Scalar
+        Bias unit after fitting.
+    losses_ : list
+      Mean squared error loss function value averaged over all
+      training examples in each epoch.
+
+
+    """
+
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        self._initialize_weights(X.shape[1])
+        self.losses_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            losses = []
+            for xi, target in zip(X, y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.losses_.append(avg_loss)
+        return self
+
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """Initialize weights to small random numbers"""
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=m)
+        self.b_ = np.float_(0.)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_ += self.eta * 2.0 * xi * (error)
+        self.b_ += self.eta * 2.0 * error
+        loss = error ** 2
+        return loss
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+ada_sgd = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada_sgd.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada_sgd)
+plt.title('Adaline - Stochastic gradient descent')
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('Petal length [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+#plt.savefig('figures/02_15_1.png', dpi=300)
+plt.show()
+
+plt.plot(range(1, len(ada_sgd.losses_) + 1), ada_sgd.losses_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average loss')
+
+#plt.savefig('figures/02_15_2.png', dpi=300)
+plt.show()
+
+
+
+
+#ada_sgd.partial_fit(X_std[0, :], y[0])
